1f360b6e1c4cf91d194313b13a772115ccd462b9
Andry Mahdison <andreymahdison@gmail.com>
2018-10-31 13:22:34 +0700

    support tomcat on main class

--- src/main/java/me/MainApplicationClass.java
+++ src/main/java/me/MainApplicationClass.java
@@ -2,9 +2,17 @@ package me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplicationClass {
+public class MainApplicationClass extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApplicationClass.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MainApplicationClass.class, args);
     }
