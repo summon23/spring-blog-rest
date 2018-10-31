21cc3188c60a8979fb4cab4c0725145b0adedec1
Andry Mahdison <andreymahdison@gmail.com>
2018-10-31 13:23:06 +0700

    repository

---
+++ src/main/java/me/BlogRepository.java
@@ -0,0 +1,13 @@
+package me;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Integer>{
+    // custom query to search to blog post by title or content
+    List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+
+}
